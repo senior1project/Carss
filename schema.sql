74576cd131e0ba9e5421d431d72dfc59921ce268
Hechmi ben sassi <hechmi.bensassi@esprit.tn>
2024-02-10 19:07:00 +0100

    back

--- schema.sql
+++ schema.sql
@@ -5,83 +5,73 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema cars
+-- Schema Rental_cars
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema cars
+-- Schema Rental_cars
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cars` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `cars` ;
+CREATE SCHEMA IF NOT EXISTS `Rental_cars` DEFAULT CHARACTER SET utf8 ;
+USE `Rental_cars` ;
 
 -- -----------------------------------------------------
--- Table `cars`.`user`
+-- Table `Rental_cars`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`user` (
+CREATE TABLE IF NOT EXISTS `Rental_cars`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cars`.`vehicule`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`vehicule` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `model` VARCHAR(255) NOT NULL,
-  `color` VARCHAR(45) NOT NULL,
-  `brand` VARCHAR(45) NOT NULL,
-  `price_by_day` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cars`.`rental`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`rental` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `start_date` DATE NOT NULL,
-  `end_date` DATE NOT NULL,
-  `total` INT NULL,
-  `user_id` INT NOT NULL,
-  `vehicule_id` INT NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(250) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_rental_user_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_rental_vehicule1_idx` (`vehicule_id` ASC) VISIBLE,
-  CONSTRAINT `fk_rental_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `cars`.`user` (`id`)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Rental_cars`.`vehicle`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Rental_cars`.`vehicle` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `model` VARCHAR(45) NOT NULL,
+  `year` INT NOT NULL,
+  `brand` VARCHAR(45) NULL,
+  `price_day` INT NULL,
+  `color` VARCHAR(45) NULL,
+  `image` VARCHAR(255) NOT NULL,
+  `desc` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Rental_cars`.`rental`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Rental_cars`.`rental` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `start_date` DATETIME NOT NULL,
+  `end_date` DATETIME NOT NULL,
+  `total` INT NULL,
+  `users_id` INT NOT NULL,
+  `vehicle_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `users_id`, `vehicle_id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `fk_rental_users_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_rental_vehicle1_idx` (`vehicle_id` ASC) VISIBLE,
+  CONSTRAINT `fk_rental_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `Rental_cars`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_rental_vehicule1`
-    FOREIGN KEY (`vehicule_id`)
-    REFERENCES `cars`.`vehicule` (`id`)
+  CONSTRAINT `fk_rental_vehicle1`
+    FOREIGN KEY (`vehicle_id`)
+    REFERENCES `Rental_cars`.`vehicle` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cars`.`admin`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`admin` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,148 +1,172 @@
-/*const Cars=require("../database-mysql/index.js")
-
-
-////////////jwt////////////
-
-  const authentication = (req, res, next)=>{
-    const token = req.header('Authorization')
-    if(!token){
-       return res.status(401)}
-    jwt.verify(token,secretKey,(err, user)=>{
-      if(err){
-        return res.status(403)}
-      req.user = user
-      next()
-    })
-  }
-
-
-///////////CRUD//////////
-const get=async(req,res)=>{
-    try{
-     const allCars = await Cars.findAll()
-      res.json(allCars)}
-    catch(error){
-      res.status(500).json(error)
-    }
-  }
-
-const post=async(req, res)=>{
-    try{
-      const newCar = await Cars.create(req.body)
-      res.json(newCar)}
-    catch (error){
-      res.status(500).json(error)
-    }
-  }
-
-const deletee=async(req,res)=>{
-    const {id} = req.params
-    try{
-      await Cars.destroy({where:{id}})
-      res.json("deleted")}
-    catch(error){
-      res.status(500).json(error)
-    }
-  }
-
-const put=async(req,res)=>{
-    const {id} = req.params
-    try{
-      await Cars.update(req.body,{where:{id}})
-      res.json("updated successfully")}
-    catch(error){
-      res.status(500).json(error)
-    }
-  }
-
-  module.exports ={authentication,get,post,deletee,put}*/
-
-  const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const car = require('../database-mysql/indexx');
-const {authentication} = require('../../server/Authentication/authentication.js') 
+const userModel = require('../database-mysql/index');
+const { authenticateToken } = require('../middlewear/token'); 
 const secretKey = process.env.JWT_SECRET || '123456789';
 
+const signup = async (req, res) => {
+  const { username, password } = req.body;
 
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.createUser(username, hashedPassword, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      console.log(results);
+      const token = jwt.sign({ username: results.username }, secretKey);
+      res.json({ message: 'User created', token });
+    });
 
-const FINDALL=(req,res)=>{
-  car.findAll((err, results)=>{
-    if(err){
-      console.log(err)} 
-    res.json(results)})
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
-const FIND=(req,res)=>{
-  let id=req.params.id
-  car.findCarById(id,(err,result)=>{
-    if(err){
-      console.log(err)}
-      res.json(result)})
-}
+const signin = async (req, res) => {
+  const { username, password } = req.body;
 
+  try {
+    const user = await userModel.findUser(username, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      console.log(results);
+      if (!results || results.length === 0) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      const match = bcrypt.compareSync(password, results[0].password);
+      if (match) {
+        const token = jwt.sign({ username: results[0].username }, secretKey);
+        res.json({ token });
+      } else {
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+    });
 
-const FINDONE=(req,res)=>{
-  let brand=req.params.brand
-  car.findCar(brand,((err,results)=>{
-    if(err){
-        console.log(err)}
-     res.json(results)}))
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
-
-const UPDATEONE=(req,res)=>{
-  let val=req.body  
-  let id=req.params.id
-  car.updateCar(val,id,((err,results)=>{
-    if(err){
-        console.log(err)}
-        res.json(results)}))
-}
-
-const DELETEONE=(req,res)=>{
-  let id=req.params.id
-  car.deleteCar(id,((err,results)=>{
-    if(err){
-        console.log(err)}
-    res.json(results)}))
-}
-
-const ADDONE=(req,res)=>{
-  let val=req.body
-  car.addCar(val,((err,results)=>{
+const FINDALL= (req,res)=>{
+  userModel.findAll( (err, results) => {
     if (err) {
-        console.log(err)}
-   res.json(results)}))
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+})
 }
 
-const Makereservation =(req,res)=>{
+
+const FINDONE = (req,res)=>{
+  let brand = req.params.brand
+  userModel.findCar(brand,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log('found : ', results)
+        res.json(results);
+    }
+}))
+}
+const FINDID = (req,res)=>{
+  let id = req.params.id
+  userModel.findByid(id,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log('found : ', results)
+        res.json(results);
+    }
+        })
+)
+}
+
+
+const UPDATEONE = (req,res)=>{
+  let val  = req.body  
+  let id =  req.params.id
+  userModel.updateCar(val,id,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+}))
+}
+
+const DELETEONE = (req,res)=>{
+
+  let id = req.params.id
+  userModel.deleteCar(id,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+}))
+}
+
+const ADDONE =(req,res)=>{
   let val= req.body
-  car.reservation(val,((err,results)=>{
-    if (err){
-        console.log(err)}
-        res.json(results)}))
-}
-
-const DELETEResrvation=(req,res)=>{
-  let id=req.params.id
-   car.deleteReservation(id,((err,results)=>{
-    if(err){
-      console.log(err)}
-    res.json(results)}))
-}
-
-const UpdateReservation=(req,res)=>{
-  let val =req.body  
-  let id=req.params.id
-  car.updateReservation(val,id,((err,results)=>{
-    if(err){
-      console.log(err)}
-    res.json(results)}))
+  userModel.addCar(val,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+}))
 }
 
 
+const RENTONE = (req,res)=>{
+  let val = req.body
+  userModel.rentCar(val,( (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+}))
+}
+const RENTS = (req,res)=>{
+  userModel.Allrents((err, results) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log( results)
+        res.json(results);
+    }
+})
+}
 
 
 
-module.exports={ADDONE,UPDATEONE,DELETEONE,FINDALL,FINDONE,FIND,UpdateReservation,
-DELETEResrvation,Makereservation}
+
+
+
+
+
+module.exports = {
+  signup,
+  signin,
+  ADDONE,
+  UPDATEONE,
+  DELETEONE,
+  FINDALL,
+  FINDONE,
+  FINDID,
+  RENTONE,
+  RENTS
+};
