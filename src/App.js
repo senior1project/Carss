563e6a9c2e8fd90c78d6a582b924a48be047e37f
eya-jouini123 <eya.jouini@etudiant-isi.utm.tn>
2024-02-10 15:42:16 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import CarInfo from "./pages/CarInfo";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+
+import Create from "./pages/Admin/Create";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route element={<Home/>} path="/" />
+        <Route element={<Contact/>} path="/contact" />
+        <Route path="/car/:id" element={<CarInfo/>}/>
+       
+        <Route path="/admin/addcar" element={<Create/>}/>
+        <Route element={<p>404 Not Found</p>} path="*"/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
